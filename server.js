5eec942a269628b8984cbe83538e4f3abb7251ea
kelsey-schenk <kelsey.schenk@att.net>
2022-01-03 18:08:40 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,31 +22,95 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Get all candiates
+// Designating the route
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+// Endpoint has a route parameter that will hold the value of the id to specify which candidate we'll select fron the database
+app.get('/api/candidate/:id', (req, res) => {
+    // Assign the captured value populated in the req.params object with the key id to params
+    // Database call will query the candidates table with this id and retrieve the row specified
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    // Params can be accepted in the database call as an arraym params is assigned as an array with a single element
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
 
 // Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        // if user tries to delete a candidate that doesn't exist
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json ({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // Create a candidate
-const sql = `INSERT INTO  candidates (id, first_name, last_name, industry_connected)
-                VALUES (?,?,?,?)`;
+// Use object destructuring to pull the body property out of the request object
+app.post('/api/candidate', ({ body }, res) => {
+    // Input check verifies that user info in the request can create a cnadidate
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
 
-const params = [1, 'Ronald', 'Firbank', 1];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });l
+});
 
 db.query(sql, params, (err, result) => {
     if (err) {
